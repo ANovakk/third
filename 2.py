49abf62f7936f086a22d86cb890917f54820a053
ANovakk <andreynovak2005@gmail.com>
2020-11-24 11:05:15 +0600

    first commit

--- 2.py
+++ 2.py
@@ -5,12 +5,41 @@ from PyQt5 import uic
 from PyQt5.QtCore import QPoint, Qt
 from PyQt5.QtGui import QPolygon, QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Second(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(490, 60, 151, 51))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Круг"))
+
+
+class Second(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('aaa.ui', self)
+        self.setupUi(self)
         self.setMouseTracking(True)
         self.coords = []
         self.qp = QPainter()
@@ -39,7 +68,7 @@ class Second(QMainWindow):
 
 
     def setBrushColor(self):
-        self.qp.setBrush(QColor(255, 255, 0))
+        self.qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
 
     def draw(self):
         self.size = random.randint(10, 100)
