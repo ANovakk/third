87dba9b939c8e98c21a0875172c499132e1a523e
ANovakk <andreynovak2005@gmail.com>
2020-11-24 15:44:38 +0600

    First commit

--- 2.py
+++ 2.py
@@ -1,81 +1,34 @@
-import random
 import sys
-
-from PyQt5 import uic
-from PyQt5.QtCore import QPoint, Qt
-from PyQt5.QtGui import QPolygon, QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QHeaderView
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import csv
+import sqlite3
 
 
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(490, 60, 151, 51))
-        self.pushButton.setObjectName("pushButton")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Круг"))
-
-
-class Second(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.setMouseTracking(True)
-        self.coords = []
-        self.qp = QPainter()
-        self.flag = False
-        self.pushButton.clicked.connect(self.run)
+        uic.loadUi('aaa.ui', self)
+        self.pushButton.clicked.connect(self.star)
+        self.con = sqlite3.connect("coffee.db")
+        self.modified = {}
+        self.titles = None
 
-    def drawf(self):
-        self.flag = True
-        self.update()
-
-    def paintEvent(self, event):
-        if self.flag:
-            self.qp = QPainter()
-            self.qp.begin(self)
-            self.setBrushColor()
-            self.draw()
-            self.qp.end()
-
-
-    def run(self):
-        self.coords = [random.randint(100, 500), random.randint(100, 500)]
-        self.drawf()
-
-    def mouseMoveEvent(self, event):
-        self.coords = [event.x(), event.y()]
+    def star(self):
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM coffees").fetchall()
+        self.tableWidget.setRowCount(len(result))
+        self.titles = ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах',
+                       'описание вкуса', 'цена', 'объем упаковки']
+        self.tableWidget.setColumnCount(len(self.titles))
+        self.tableWidget.setHorizontalHeaderLabels(self.titles)
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
 
 
-    def setBrushColor(self):
-        self.qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-
-    def draw(self):
-        self.size = random.randint(10, 100)
-        self.qp.drawEllipse(*self.coords, self.size, self.size)
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Second()
-    ex.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
